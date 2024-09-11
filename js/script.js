b7ca9a9fdb5b75349040a9c0ab056f389262b52a
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-11 16:22:49 +0200

    collegata l'API per generare 10 email

--- js/script.js
+++ js/script.js
@@ -6,4 +6,23 @@ createApp({
       emails: []
     };
   },
+  mounted() {
+    this.generateEmails();
+  },
+  methods: {
+    generateEmails() {
+      for (let i = 0; i < 10; i++) {
+        //richiesta http
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          //per ogni chiamata API viene aggiunta all'interno dell'array
+          .then(response => {
+            this.emails.push(response.data.response);
+          })
+          //gestione errori
+          .catch(error => {
+            console.error('Non ci sono email:', error);
+          });
+      }
+    }
+  }
 }).mount('#app');
